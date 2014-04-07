ec8d8c265bf8abc34c5292a0e66047d12edf82a7
Yu-Shan Sun <yushans@g.clemson.edu>
2014-04-07 19:08:16 -0400

    updates for Stephen's class

---
+++ RESOLVE/Main/Concepts/Sequence_Template/Skeleton_Realiz.rb
@@ -0,0 +1,24 @@
+Realization Skeleton_Realiz for Sequence_Template;
+	
+	Type Sequence = Record 
+			Dummy1: Integer;
+			Dummy2: Integer;      
+		end;
+	end;
+    
+	Procedure Insert_after(evaluates Pos: Integer; alters E: Entry; updates S: Sequence);
+	end Insert_after;
+
+	Procedure Remove_after(evaluates Pos: Integer; replaces R: Entry; updates S: Sequence);
+	end Remove_after;
+
+	Procedure Length(restores S: Sequence): Integer;
+	end Length;
+ 
+	Procedure Rem_Capacity(restores S: Sequence): Integer;
+	end Rem_Capacity;
+ 
+	Procedure Clear(clears S: Sequence);
+	end Clear;
+
+end Skeleton_Realiz;
