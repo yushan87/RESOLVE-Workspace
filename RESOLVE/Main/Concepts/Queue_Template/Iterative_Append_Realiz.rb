f1d5f994b3ada5b6a1c2fd06065cceb99972ceb7
Yu-Shan Sun <yushans@g.clemson.edu>
2015-04-17 11:01:31 -0400

    Exercise for the 828 students

--- RESOLVE/Main/Concepts/Queue_Template/Iterative_Append_Realiz.rb
+++ RESOLVE/Main/Concepts/Queue_Template/Iterative_Append_Realiz.rb
@@ -1,12 +1,17 @@
 Realization Iterative_Append_Realiz for Append_Capability of Queue_Template;
+    -- will not verify because loop invariant is adequate
     Procedure Append(updates P: Queue; clears Q: Queue);
         Var E: Entry;
-        While ( Is_Not_Zero(Length(Q)) )
-          maintaining (P o Q = #P o #Q);
+        Var Len: Integer;
+        Len := Length(Q);
+        While ( Is_Not_Zero(Len) )
+          -- fill in a suitable invariant
+          maintaining true;
           decreasing |Q|;
         do
           Dequeue(E,Q);
           Enqueue(E,P);
+          Len := Length(Q);
         end;
     end Append;
 end Iterative_Append_Realiz;
