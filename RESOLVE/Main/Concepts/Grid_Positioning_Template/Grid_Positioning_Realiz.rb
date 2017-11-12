0f0f4c2fe7c5f1f963f4b956a7528b13732dbb62
Yu-Shan Sun <yushans@g.clemson.edu>
2017-11-12 08:57:39 -0500

    Added instructions to Grid_Positioning_Realiz

--- RESOLVE/Main/Concepts/Grid_Positioning_Template/Grid_Positioning_Realiz.rb
+++ RESOLVE/Main/Concepts/Grid_Positioning_Template/Grid_Positioning_Realiz.rb
@@ -1,10 +1,24 @@
 Realization Grid_Positioning_Realiz for Grid_Positioning_Template;
+	-- Activity to understand correspondence assertion
+	-- Attempt to verify this code and it should verify
+	-- The concrete coordinates in the implementation do not need to correspond directly to the abstract coordinates in the concept
+	--
+	-- Activity: Comment the present correspondence and uncomment the given alternative correspondence
+	-- Rewrite the code for where necessary, starting with initialization, and verify
 
 	Type Grid_Pos = Record
 			X, Y: Integer;
 		end;
 		conventions 0 <= GP.X <= Max_Right and 0 <= GP.Y <= Max_Top;
+		-- correspondence Conc.GP.X = Max_Right - GP.X and Conc.GP.Y = GP.Y;
 		correspondence Conc.GP.X = GP.X and Conc.GP.Y = GP.Y;
+	(*
+		-- initialization code below is redundant because an Integer's initial value is 0
+		initialization
+			GP.X := 0;
+			GP.Y := 0;
+		end;
+	*)
 	end;
 
 	Procedure Move_Left(updates GP: Grid_Pos);
